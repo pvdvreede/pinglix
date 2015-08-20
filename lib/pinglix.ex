aef4e38e2e96668e2f2f32ad99cfc80d6c28a2ff
pvdvreede <paul@vdvreede.net>
2015-08-20 12:15:28 +1000

    Move response into its own private method.

--- lib/pinglix.ex
+++ lib/pinglix.ex
@@ -9,11 +9,15 @@ defmodule Pinglix do
   def call(conn = %Plug.Conn{path_info: ["_ping"], method: "GET"}, _opts) do
     conn
     |> put_resp_content_type("application/json", "UTF-8")
-    |> send_resp(200, Poison.encode!(%{status: "ok", now: current_time}, pretty: true))
+    |> send_resp(200, response)
   end
 
   def call(conn, _opts) do
     conn
+  end
+
+  defp response do
+    Poison.encode!(%{status: "ok", now: current_time}, pretty: true)
   end
 
   defp current_time do
