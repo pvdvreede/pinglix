b2c3ff2bbc7bb9ecf9dda09bcd4475c89d91e163
Kian-Meng, Ang <kianmeng@cpan.org>
2021-08-15 20:08:36 +0800

    Misc doc changes
    Besides other documentation changes, this commit ensures the generated
    HTML doc for HexDocs.pm will become the source of truth for this Elixir
    library and leverage on latest features of ExDoc.

--- mix.exs
+++ mix.exs
@@ -1,52 +1,58 @@
 defmodule Pinglix.Mixfile do
   use Mix.Project
 
+  @source_url "https://github.com/pvdvreede/pinglix"
+  @version "1.1.4"
+
   def project do
-    [app: :pinglix,
-     description: description(),
-     package: package(),
-     version:  "1.1.4",
-     elixir: "~> 1.6",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :pinglix,
+      version: @version,
+      elixir: "~> 1.6",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      package: package(),
+      deps: deps(),
+      docs: docs()
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [extra_applications: [:logger]]
   end
 
-  defp description do
-    "Plug compatible health check system in Elixir based on https://github.com/jbarnette/pinglish."
-  end
-
   defp package do
     [
+      description:
+        "Plug compatible health check system in Elixir based " <>
+          "on https://github.com/jbarnette/pinglish.",
       contributors: ["Paul Van de Vreede"],
       maintainers: ["Paul Van de Vreede"],
-      links: %{"Github" => "https://github.com/pvdvreede/pinglix"},
       licenses: ["MIT License"],
-      files: ["lib", "mix.exs", "README.md", "LICENSE"]]
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      links: %{"GitHub" => @source_url}
+    ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:ex_doc, "~> 0.20", only: :dev},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:dialyxir, "~> 0.5", only: :dev},
       {:timex, "~> 3.0"},
       {:poison, "~> 3.1.0"},
-      {:plug, "~> 1.0"}]
+      {:plug, "~> 1.0"}
+    ]
+  end
+
+  defp docs do
+    [
+      extras: [
+        "LICENSE.md": [title: "License"],
+        "README.md": [title: "Overview"]
+      ],
+      main: "readme",
+      source_url: @source_url,
+      formatters: ["html"]
+    ]
   end
 end
